d9d225c5d7775fd6590bf9b9c6cea15891dd492e
johncjwood <johncjwood@gmail.com>
2014-12-07 17:00:17 -0500

    Update 3

--- Plot1.R
+++ Plot1.R
@@ -1,10 +1,16 @@
+#, colClasses = c("Date","Time","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
 ds <- read.table("C:\\Coursera\\household_power_consumption.txt", sep = ";", header = TRUE)
+ds1 <- subset( ds, as.Date(ds$Date, "%d/%m/%Y") >= as.Date("2007-02-01") )
+ds2 <- subset( ds1, as.Date(ds1$Date, "%d/%m/%Y") <= as.Date("2007-02-02") )
+head(ds2)
 
-hist(as.numeric(ds$Global_active_power)/1000,col="red",main="Global Active Power",breaks=12
+hist(as.numeric(ds2$Global_active_power)/1000,col="red",main="Global Active Power",breaks=12
      ,xlab="Global Active Power (killowatts)", ylab="Frequency"
-     ,xlim=c(0, 6), ylim=c(0, 1600000)
+     ,xlim=c(0, 6) , ylim=c(0, 1200)
      ,axes=FALSE)
 axis(1,pos=c(0,0),at=seq(0,6,by=2))
 axis(2)
+
+
 dev.copy(png, file = "C:\\Coursera\\plot1.png",width = 480, height = 480)
 dev.off() ##close png device
--- plot2.R
+++ plot2.R
@@ -1,10 +1,19 @@
-ds <- read.table("C:\\Coursera\\household_power_consumption.txt", sep = ";", header = TRUE, colClasses = c("Date","Time","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+#, colClasses = c("Date","Time","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
+ds <- read.table("C:\\Coursera\\household_power_consumption.txt", sep = ";", header = TRUE)
+ds1 <- subset( ds, as.Date(ds$Date, "%d/%m/%Y") >= as.Date("2007-02-01") )
+ds2 <- subset( ds1, as.Date(ds1$Date, "%d/%m/%Y") <= as.Date("2007-02-02") )
+#head(as.POSIXct(paste(ds2$Date,ds2$Time), format = "%d/%m/%Y %H:%M:%S"))
+#head(as.numeric(ds2$Global_active_power)/1000)
 
-hist(as.numeric(ds$Global_active_power)/1000,col="red",main="Global Active Power",breaks=12
-     ,xlab="Global Active Power (killowatts)", ylab="Frequency"
-     ,xlim=c(0, 6), ylim=c(0, 1600000)
-     ,axes=FALSE)
-axis(1,pos=c(0,0),at=seq(0,6,by=2))
-axis(2)
-dev.copy(png, file = "C:\\Coursera\\plot1.png",width = 480, height = 480)
+plot(
+  as.POSIXct(paste(ds2$Date,ds2$Time), format = "%d/%m/%Y %H:%M:%S")
+     ,as.numeric(ds2$Global_active_power)/1000
+     ,col="black"
+     ,ylab="Global Active Power (killowatts)"
+     ,xlim=c(as.Date("2007-01-01"), as.Date("2007-02-03"))
+     #,ylim=c(0, 6)  ## with c()
+     #, type="l"
+)
+dev.copy(png, file = "C:\\Coursera\\plot2.png",width = 480, height = 480)
 dev.off() ##close png device
+
